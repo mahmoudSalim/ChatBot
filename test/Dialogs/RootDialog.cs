270ef9bb6b14e2d2e6a741ef2393f1fa1a85a19c
mahmoud <mahmoudsalim553@yahoo.com>
2017-08-05 01:27:20 +0200

    Add project files.

---
+++ test/Dialogs/RootDialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace test.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            String question = result.ToString;
+
+            switch (question)
+            {
+                case question.Contains("register" && "Internet Banking"):
+                    await context.PostAsync("You can simply register for free on Internet Banking by using your" +
+                        " ATM 16 Digits Debit Card number and its 4 Digits PIN. Simply go to Internet Banking" +
+                        " Registration page and then choose \" Register New User\".Register now to the Internet" +
+                        " banking Service to get access to your banking accounts from anywhere 24 / 7. ");
+
+            }
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
